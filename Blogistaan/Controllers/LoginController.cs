57524893e89bb4775840b438b8ce1db61419b031
Ata Ul Mohsin <57590526+aumohsin99@users.noreply.github.com>
2023-05-29 19:22:49 +0500

    hardcoded redirection done

--- Blogistaan/Controllers/LoginController.cs
+++ Blogistaan/Controllers/LoginController.cs
@@ -1,21 +1,59 @@
 ﻿using Blogistaan.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Blogistaan.Controllers
 {
     public class LoginController : Controller
     {
 
-      
 
+        [HttpGet]
         public IActionResult WriterLogin()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult WriterLogin(Writer writerobj)
+        {
+            // Handle login logic here
+            if (writerobj.Username == "w1" && writerobj.Password=="1") 
+            {
+                return RedirectToAction("Dashboard", "Writer");
+
+                //return View("Writer/Dashboard");
+
+            }
+            else
+            {
+                return View();
+
+            }
+
+
+            //return View();
+        }
+
+        [HttpGet]
         public IActionResult AdminLogin()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AdminLogin(Admin adminobj)
+        {
+
+            if (adminobj.Username == "a1" && adminobj.Password == "1")
+            {
+                return RedirectToAction("Dashboard", "Admin");
+            }
+            else
+            {
+                return View();
+
+            }
         }
 
        
---
+++ Blogistaan/Repository/WriterRepo.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Blogistaan.Models;
+
+namespace Blogistaan.Repository
+{
+    public class WriterRepo
+    {
+        Writer writerobj = new Writer();
+    
+
+
+
+
+    }
+}
